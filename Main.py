d683491eb08a83ab4edf721f9505aa44022ceab5
Anderwsont_RevelLux <guidomburgos@gmail.com>
2026-07-06 18:20:33 -0300

    separacion de funciones en archivos independientes, que luego se importan en main

--- Main.py
+++ Main.py
@@ -1,82 +1,8 @@
 import Logo_Inicio as LG
 import funciones_globales as FG
-    
-def menu_servicios():
-    while True:
-        FG.limpiar_pantalla()
-        print("~" * 40)
-        print("        Gestión de Servicios")
-        print("~" * 40)
-        print("1. Agregar servicio")
-        print("2. Listar servicios")
-        print("0. Volver al Menú Principal")
-        print("~" * 40)
-        opcion = input("Ingrese la opción deseada: ")
-        match opcion:
-            case "1":
-                FG.limpiar_pantalla()
-                print("--- Registrar Nuevo Servicio ---")
-                print("\n[Simulación] Ingrese nombre: Baño y Corte")
-                print("[Simulación] Ingrese precio: $12000")
-                print("\n-> ¡Servicio guardado con éxito (Simulado)!")
-                pausar()
-            case "2":
-                FG.limpiar_pantalla()
-                print("--- Lista de Servicios Disponibles ---")
-                print("-" * 40)
-                print("ID   | Detalle             | Precio")
-                print("-" * 40)
-                print("001  | Consulta Médica     | $8500")
-                print("002  | Vacunación Quinta   | $11000")
-                print("003  | Baño y Peluquería   | $12000")
-                print("004  | Desparasitación     | $5000")
-                print("-" * 40)
-                FG.pausar()
-            case "0":
-                break
-            case _:
-                print("\nOpción inválida.")
-                FG.pausar()
 
-def menu_estadisticas():
-    while True:
-        FG.limpiar_pantalla()
-        print("~" * 40)
-        print("            Estadísticas")
-        print("~" * 40)
-        print("1. Servicios más frecuentes")
-        print("2. Motivos de consulta más frecuentes")
-        print("3. Mascotas más atendidas")
-        print("0. Volver al Menú Principal")
-        print("~" * 40)
-        opcion = input("Ingrese la opción deseada: ")
-        match opcion:
-            case "1":
-                FG.limpiar_pantalla()
-                print("--- Ranking de Servicios Más Solicitados ---")
-                print("\n1. Consulta Médica Gral. (45 veces)")
-                print("2. Baño y Peluquería     (32 veces)")
-                print("3. Vacunación            (28 veces)")
-                FG.pausar()
-            case "2":
-                FG.limpiar_pantalla()
-                print("--- Principales Motivos de Consulta ---")
-                print("\n1. Control anual / Vacunas  (50%)")
-                print("2. Problemas en la piel     (30%)")
-                print("3. Accidentes / Urgencias   (20%)")
-                FG.pausar()
-            case "3":
-                FG.limpiar_pantalla()
-                print("--- Mascotas con Mayor Cantidad de Atenciones ---")
-                print("\n1. 'Thor' (Perro - Golden Retriever) -> 8 visitas")
-                print("2. 'Luna' (Gato - Siamés)            -> 6 visitas")
-                print("3. 'Rocco' (Perro - Caniche)         -> 5 visitas")
-                FG.pausar()
-            case "0":
-                break
-            case _:
-                print("\nOpción inválida.")
-                FG.pausar()
+from menu_servicios import menu_servicios    
+from menu_estadisticas import menu_estadisticas
 
 def menu_principal():
     while True:
@@ -95,7 +21,6 @@ def menu_principal():
         print("~" * 40)
 
         opcion = input("Ingrese la opcion deseada:")
-
 
         match opcion:
             case "1":
---
+++ menu_estadisticas.py
@@ -0,0 +1,41 @@
+import funciones_globales as FG
+
+def menu_estadisticas():
+    while True:
+        FG.limpiar_pantalla()
+        print("~" * 40)
+        print("            Estadísticas")
+        print("~" * 40)
+        print("1. Servicios más frecuentes")
+        print("2. Motivos de consulta más frecuentes")
+        print("3. Mascotas más atendidas")
+        print("0. Volver al Menú Principal")
+        print("~" * 40)
+        opcion = input("Ingrese la opción deseada: ")
+        match opcion:
+            case "1":
+                FG.limpiar_pantalla()
+                print("--- Ranking de Servicios Más Solicitados ---")
+                print("\n1. Consulta Médica Gral. (45 veces)")
+                print("2. Baño y Peluquería     (32 veces)")
+                print("3. Vacunación            (28 veces)")
+                FG.pausar()
+            case "2":
+                FG.limpiar_pantalla()
+                print("--- Principales Motivos de Consulta ---")
+                print("\n1. Control anual / Vacunas  (50%)")
+                print("2. Problemas en la piel     (30%)")
+                print("3. Accidentes / Urgencias   (20%)")
+                FG.pausar()
+            case "3":
+                FG.limpiar_pantalla()
+                print("--- Mascotas con Mayor Cantidad de Atenciones ---")
+                print("\n1. 'Thor' (Perro - Golden Retriever) -> 8 visitas")
+                print("2. 'Luna' (Gato - Siamés)            -> 6 visitas")
+                print("3. 'Rocco' (Perro - Caniche)         -> 5 visitas")
+                FG.pausar()
+            case "0":
+                break
+            case _:
+                print("\nOpción inválida.")
+                FG.pausar()
