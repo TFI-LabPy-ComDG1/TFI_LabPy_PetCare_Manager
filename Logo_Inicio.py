7d2ddfaa70f6500af072032f7d5ae989abba8955
Anderwsont_RevelLux <guidomburgos@gmail.com>
2026-07-06 19:42:40 -0300

    carga de logos en imagenes, y modificacion de logo_inicio.py

--- Logo_Inicio.py
+++ Logo_Inicio.py
@@ -4,15 +4,15 @@ def logo_inicio():
     FG.limpiar_pantalla()
     print(" ||========================================|| ")
     print(" ||                                        || ")
+    print(" ||                 /\_/\                  || ")
+    print(" ||            ♥  =( °w° )=   ♥            || ")
+    print(" ||                 )   (                  || ")
+    print(" ||                (__ __)                 || ")
     print(" ||                                        || ")
-    print(" ||                                        || ")
-    print(" ||                                        || ")
-    print(" ||                                        || ")
-    print(" ||                                        || ")
+    print(" ||            PET CARE MANAGER            || ")
     print(" ||                                        || ")
     print(" ||========================================|| ")
 
 ##LLAMADO DE FUNCION
 logo_inicio()
 FG.pausar()
-
