ed5669169fb4185ca98eadc7d50b2a98e93be6e5
Anderwsont_RevelLux <guidomburgos@gmail.com>
2026-07-08 12:03:53 -0300

    creacion de pantalla de fin

---
+++ logos/logo_fin.py
@@ -0,0 +1,24 @@
+from utils import funciones_globales as FG
+
+def Logo_UTN_fin():
+    FG.limpiar_pantalla()
+    print(r"  ██==============================================██  ")
+    print(r"  ||                                              ||  ")
+    print(r"  ||                ▄     ▄▄     ▄                ||  ")
+    print(r"  ||                ▀█    ██    █▀                ||  ")
+    print(r"  ||                 ▀█▄▄ ██ ▄▄█▀                 ||  ")
+    print(r"  ||                    █▄██▄█                    ||  ")
+    print(r"  ||                ██████████████                ||  ")
+    print(r"  ||                    █▀██▀█                    ||  ")
+    print(r"  ||                 ▄█▀▀ ██ ▀▀█▄                 ||  ")
+    print(r"  ||                ▄█    ██    █▄                ||  ")
+    print(r"  ||                ▀     ▀▀     ▀                ||  ")
+    print(r"  ||                                              ||  ")
+    print(r"  ██==============================================██  ")
+    print("\n             >> PROROGRAMA  FINALIZADO <<             ")
+    print("\n           >> PRESIONA ENTER PARA CERRAR <<           ")
+    FG.pausar()
+
+if __name__ == "__main__":
+    Logo_UTN_fin()
+
